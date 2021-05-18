dc03a438a05c2402d161e10225b919c2543e67f4
poojanehra <poojanehra444@gmail.com>
2021-05-17 20:04:30 -0400

    Java Program

---
+++ src/SessionTwo.java
@@ -0,0 +1,42 @@
+public class SessionTwo {
+    public static void main(String[] args) {
+        int populationOfMississauga ;
+        populationOfMississauga = 700000;
+
+        int a=1000;
+        int b= 2000;
+        short sum= 5;
+        short total= 10;
+        long totalValue = 134578;
+        long finalAmount = 35796;
+        System.out.println(a + " " + b + " " + sum + " " + total + " " + totalValue);
+        int amount= a + 1000;
+        System.out.println("amount " + amount);
+        double value = 10.26439;
+        System.out.println(value);
+        float pi = 3.01f;
+        System.out.println(pi);
+        double salesValue = 1234.23472842637892;
+        float profitValue = 12.2537728956f;
+        System.out.println(salesValue + " " + profitValue);
+        double d = 12.89;
+        float f = 34.12345f;
+        System.out.println("Double " + d + " Float " + f);
+        boolean isIndian = false;
+        char c = 'I';
+        System.out.println(isIndian);
+        System.out.println(c);
+        byte data = 123;
+        System.out.println();
+
+
+
+
+
+
+
+
+
+
+    }
+}
